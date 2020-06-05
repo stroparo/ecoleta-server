d612677f7dac0d899f34ef9664b6fd9b39232e53
Cristian Stroparo <cstropz@gmail.com>
2020-06-05 20:05:03 -0300

    Update exercise day 2 of the nlw event.

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,41 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+  'Mr. L. Daher',
+  'Mr. Jay M.',
+  'Cpt. Stropz'
+];
+
 app.get('/users', (request, response) => {
-  console.log('Users! Yay!')
-  response.json([
-    'Mr. L. Daher',
-    'Mr. Jay M.',
-    'Cpt. Stropz'
-  ]);
+  const search = String(request.query.search);
+
+  const usersFiltered = users.filter(user => search ? user.includes(search) : users);
+
+  return response.json(usersFiltered);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  console.log(data);
+
+  const user = {
+    name: data.name,
+    email: data.email
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
